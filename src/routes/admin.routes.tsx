6b97a42f619ff30ffd88cd557c48b1c950e98e27
Melon-ali <86640181+Melon-ali@users.noreply.github.com>
2025-01-07 09:12:27 +0600

    Refactor hard coded admin routes added

--- src/routes/admin.routes.tsx
+++ src/routes/admin.routes.tsx
@@ -1,9 +1,15 @@
+import { ReactNode } from 'react'
 import AdminDashboard from '../pages/admin/AdminDashboard'
 import CreateAdmin from '../pages/admin/CreateAdmin'
 import CreateFaculty from '../pages/admin/CreateFaculty'
 import CreateStudent from '../pages/admin/CreateStudent'
 
-const adminPaths2 = [
+type TRoute = {
+    path: string,
+    element: ReactNode
+}
+
+const adminPaths = [
   {
     name: 'Dashboard',
     path: '/admin/dashboard',
@@ -29,14 +35,23 @@ const adminPaths2 = [
       },
     ],
   },
-  {
-    name: 'Course Management',
-    children: [
-      {
-        name: 'Offered Course',
-        path: '/admin/create-admin',
-        element: <CreateAdmin />,
-      },
-    ],
-  },
 ]
+
+export const adminRoutes = adminPaths.reduce((acc: TRoute[], item) => {
+    if(item.path && item.element) {
+        acc.push({
+            path: item.path,
+            element: item.element
+        })
+    }
+
+    if(item.children){
+        item.children.forEach((child) => {
+            acc.push({
+                path: child.path,
+                element: child.element
+            })
+        })
+    }
+    return acc;
+}, [])
