6c7cabf37e3122aa26cdcac1b4f3dac2cad00698
Melon-ali <86640181+Melon-ali@users.noreply.github.com>
2025-01-10 17:11:03 +0600

    Decode token and set user in local state added

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,29 +1,36 @@
-import { Button } from "antd";
-import { useForm } from "react-hook-form"
-import { useLoginMutation } from "../redux/features/auth/authApi";
-
+import { Button } from 'antd'
+import { useForm } from 'react-hook-form'
+import { useLoginMutation } from '../redux/features/auth/authApi'
+import { useAppDispatch } from '../redux/hooks'
+import { setUser } from '../redux/features/auth/authSlice'
+import { verifyToken } from '../utils/verifyToken'
 
 const Login = () => {
-  const {register, handleSubmit} = useForm({
+  const dispatch = useAppDispatch()
+  const { register, handleSubmit } = useForm({
     defaultValues: {
-      userId: 'A-0002',
-      password: 'admin123'
-    }
-  });
+      userId: 'A-0001',
+      password: 'admin123',
+    },
+  })
 
-  const [login, {data, error}] = useLoginMutation();
+  const [login, { data, error }] = useLoginMutation()
 
-  console.log('data => ', data);
-  console.log('error => ', error);
+  console.log('data => ', data)
+  console.log('error => ', error)
 
-  const onSubmit = (data) => {
-    const userInfo =  {
+  const onSubmit = async (data) => {
+    const userInfo = {
       id: data.userId,
       password: data.password,
-    };
-    login(userInfo)
+    }
+    const res = await login(userInfo).unwrap()
+    const user = verifyToken(res.data.accessToken)
+    console.log(user)
+
+    dispatch(setUser({ user: user, token: res.data.accessToken }))
   }
-  
+
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <div>
