c4fc7f4952539a21522a6cc0986d5e1200652b83
Melon-ali <86640181+Melon-ali@users.noreply.github.com>
2025-01-14 12:22:34 +0600

    Handle expired refresh token (kick out the user) added

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,45 +1,48 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { RootState } from "../store";
-import { setUser } from "../features/auth/authSlice";
+import { logout, setUser } from "../features/auth/authSlice";
 
-const baseQuery = fetchBaseQuery({ baseUrl: "http://localhost:5000/api/v1",
-  credentials: 'include',
-  prepareHeaders: (headers, {getState}) => {
+const baseQuery = fetchBaseQuery({
+  baseUrl: "http://localhost:5000/api/v1",
+  credentials: "include",
+  prepareHeaders: (headers, { getState }) => {
     const token = (getState() as RootState).auth.token;
 
-    if(token){
-      headers.set('authorization', `${token}`)
+    if (token) {
+      headers.set("authorization", `${token}`);
     }
-    return headers
-  }
- })
+    return headers;
+  },
+});
 
- const baseQueryWithRefreshToken = async (args, api, extraOptions) => {
-  const result = await baseQuery(args, api, extraOptions);
-  console.log(result);
+const baseQueryWithRefreshToken = async (args, api, extraOptions) => {
+  let result = await baseQuery(args, api, extraOptions);
+  
 
-  if(result?.error?.status === 401){
+  if (result?.error?.status === 401) {
     //* Send Refresh
-    console.log('Sending refresh token');;
-    const res = await fetch('http://localhost:5000/api/v1/auth/refresh-token', {
-      method: 'POST',
-      credentials: 'include'
-    })
+    console.log("Sending refresh token");
+    const res = await fetch("http://localhost:5000/api/v1/auth/refresh-token", {
+      method: "POST",
+      credentials: "include",
+    });
 
     const data = await res.json();
-    console.log(data);
     const user = (api.getState() as RootState).auth.user;
 
-    api.dispath(
+    api.dispatch(
       setUser({
-        user, 
-        token: data.data.accessToken,
+        user,
+        token: data?.data?.accessToken,
       })
-    )
+    );
+    result = await baseQuery(args, api, extraOptions);
+  } else {
+    api.dispatch(logout());
   }
 
-  return result
- }
+  return result;
+};
 
 export const baseApi = createApi({
   reducerPath: "baseApi",
