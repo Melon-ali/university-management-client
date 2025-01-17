1154068aecc49bedc18f6d908a90b6e8aa2cb300
Melon-ali <86640181+Melon-ali@users.noreply.github.com>
2025-01-17 10:14:18 +0600

    Refactor Form component added

--- src/components/form/PHInput.tsx
+++ src/components/form/PHInput.tsx
@@ -1,19 +1,22 @@
-import { Input } from 'antd'
+import { Form, Input } from 'antd'
 import { Controller } from 'react-hook-form'
 
 type IInputProps = {
-    type: string;
-    name: string;
-    label?: string;
+  type: string
+  name: string
+  label?: string
 }
 
 const PHInput = ({ type, name, label }: IInputProps) => {
   return (
-    <div style={{marginBottom: '20px'}}>
-      {label ? label : null}
+    <div style={{ marginBottom: '20px' }}>
       <Controller
         name={name}
-        render={({ field }) => <Input {...field} type={type} id={name} />}
+        render={({ field }) => (
+          <Form.Item label={label}>
+            <Input {...field} type={type} id={name} />
+          </Form.Item>
+        )}
       />
     </div>
   )
--- src/pages/admin/academicManagement/CreateAcademicSemester.tsx
+++ src/pages/admin/academicManagement/CreateAcademicSemester.tsx
@@ -1,20 +1,25 @@
-import { FieldValues, SubmitHandler } from "react-hook-form";
-import PHForm from "../../../components/form/PHForm"
-import PHInput from "../../../components/form/PHInput"
-import { Button } from "antd";
-
+import { FieldValues, SubmitHandler } from 'react-hook-form'
+import PHForm from '../../../components/form/PHForm'
+import PHInput from '../../../components/form/PHInput'
+import { Button, Col, Flex } from 'antd'
+import PHSelect from '../../../components/form/PHSelect'
 
 const CreateAcademicSemester = () => {
-
-    const onSubmit: SubmitHandler<FieldValues> = (data) => {
-        console.log(data);
-    }
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    console.log(data)
+  }
 
   return (
-    <PHForm onSubmit={onSubmit}>
-        <PHInput type="text" name="name" />
-        <Button htmlType="submit">Submit</Button>
-    </PHForm>
+    <Flex justify="center" align="center">
+      <Col span={6}>
+        <PHForm onSubmit={onSubmit}>
+          <PHInput type="text" name="name" label="Name" />
+          <PHInput type="text" name="name" label="Year" />
+          <PHSelect label="Name" />
+          <Button htmlType="submit">Submit</Button>
+        </PHForm>
+      </Col>
+    </Flex>
   )
 }
 
