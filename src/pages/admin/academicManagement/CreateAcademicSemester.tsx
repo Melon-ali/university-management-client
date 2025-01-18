e592c7571224a61e7dd59eea63884d5a51a45425
Melon-ali <86640181+Melon-ali@users.noreply.github.com>
2025-01-18 17:39:28 +0600

    Add academic semester using RTK Query Mutation added

--- src/pages/admin/academicManagement/CreateAcademicSemester.tsx
+++ src/pages/admin/academicManagement/CreateAcademicSemester.tsx
@@ -6,6 +6,8 @@ import { semesterOptions } from '../../../constants/semester'
 import { monthOptions } from '../../../constants/global'
 import {zodResolver} from '@hookform/resolvers/zod'
 import { academicSemesterSchema } from '../../../schemas/academicManagement.schema'
+import { useAddAcademicSemesterMutation } from '../../../redux/features/admin/academicManagement.api'
+import { toast } from 'sonner'
 
 
 const currentYear = new Date().getFullYear();
@@ -16,7 +18,9 @@ const yearOptions = [0, 1, 2, 3, 4,].map((number) => ({
 console.log(yearOptions);
 
 const CreateAcademicSemester = () => {
-  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+  const [addAcademicSemester] = useAddAcademicSemesterMutation();
+
+  const onSubmit: SubmitHandler<FieldValues> = async (data) => {
     const name = semesterOptions[Number(data?.name) - 1]?.label;
 
     const semesterData = {
@@ -27,7 +31,13 @@ const CreateAcademicSemester = () => {
       endMonth: data.endMonth,
     }
 
-    console.log(semesterData)
+    try {
+      console.log(semesterData)
+      const res = await addAcademicSemester(semesterData)
+      console.log(res);
+    } catch (error) {
+      toast.error('Something went wrong')
+    }
   }
 
   return (
