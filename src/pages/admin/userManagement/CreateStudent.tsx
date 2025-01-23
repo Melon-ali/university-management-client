5405eabba02f435f5cfe0f37ad959a9311505348
Melon-ali <86640181+Melon-ali@users.noreply.github.com>
2025-01-23 11:28:47 +0600

    Create Student basic form structure added

--- src/pages/admin/userManagement/CreateStudent.tsx
+++ src/pages/admin/userManagement/CreateStudent.tsx
@@ -1,7 +1,7 @@
 import { FieldValues, SubmitHandler } from 'react-hook-form'
 import PHForm from '../../../components/form/PHForm'
 import PHInput from '../../../components/form/PHInput'
-import { Button } from 'antd'
+import { Button, Col, Divider, Row } from 'antd'
 
 const studentDummyData = {
   password: 'student123',
@@ -44,17 +44,42 @@ const studentDummyData = {
 
 const CreateStudent = () => {
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
-    const formData = new FormData();
+    console.log(data);
+    // const formData = new FormData()
 
-    formData.append('data', JSON.stringify(data))
+    // formData.append('data', JSON.stringify(data))
 
-    console.log(Object.fromEntries(formData))
+    // console.log(Object.fromEntries(formData))
   }
   return (
-    <PHForm onSubmit={onSubmit}>
-      <PHInput type="text" name="name" label="Name" />
-      <Button htmlType='submit'>Submit</Button>
-    </PHForm>
+    <Row>
+      <Col span={24}>
+        <PHForm onSubmit={onSubmit}>
+          <Divider>Personal Info.</Divider>
+          <Row gutter={8}>
+            <Col span={24} md={{span: 12}} lg={{span: 8}}>
+              <PHInput type="text" name="name.firstName" label="First Name" />
+            </Col>
+            <Col span={24} md={{span: 12}} lg={{span: 8}}>
+              <PHInput type="text" name="name.middleName" label="Middle Name" />
+            </Col>
+            <Col span={24} md={{span: 12}} lg={{span: 8}}>
+              <PHInput type="text" name="name.lastName" label="Last Name" />
+            </Col>
+            <Col span={24} md={{span: 12}} lg={{span: 8}}>
+              <PHInput type="text" name="gender" label="First Name" />
+            </Col>
+            <Col span={24} md={{span: 12}} lg={{span: 8}}>
+              <PHInput type="text" name="dateOfBirth" label="Middle Name" />
+            </Col>
+            <Col span={24} md={{span: 12}} lg={{span: 8}}>
+              <PHInput type="text" name="bloogGroup" label="Last Name" />
+            </Col>
+          </Row>
+          <Button htmlType="submit">Submit</Button>
+        </PHForm>
+      </Col>
+    </Row>
   )
 }
 
